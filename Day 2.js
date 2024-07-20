a4078e530551a829b42476674f8744cb45e75be2
SHIVAM-XZ <soni.shivam0037@gmail.com>
2024-07-20 18:21:32 +0530

    14 july

---
+++ Day 2.js
@@ -0,0 +1,74 @@
+// Day 2: Operators
+
+Task1:
+x=2;
+y=3;
+console.log(x + y)
+
+Task2:
+x=2;
+y=3;
+console.log(x - y)
+
+Task3:
+x=2;
+y=3;
+console.log(x * y)
+
+Task4:
+x=2;
+y=3;
+console.log(x / y)
+
+Task5:
+x=2;
+y=3;
+console.log(x % y)
+
+Task6:
+x=2;
+x+=3;
+console.log(x)
+
+Task7:
+x=2;
+x-=3;
+console.log(x)
+
+Task8:
+let x=2;
+let y=3;
+console.log(x > y)
+console.log(x < y)
+
+Task9:
+let x=2;
+let y=3;
+console.log(x >= y)
+console.log(x <= y)
+
+Task10:
+let x=2;
+let y=3;
+console.log(x == y)
+console.log(x === y)
+
+Task11:
+let x=2;
+let y=3;
+console.log(x>y && x===y)
+
+Task12:
+let x=2;
+let y=3;
+console.log(x<y || x==y)
+
+Task13:
+let x=2;
+let y=3;
+console.log(x != y)
+
+Task14:
+let num = 1;
+const number = (num > 0) ? "positive number":"negative number";
+console.log(number)
