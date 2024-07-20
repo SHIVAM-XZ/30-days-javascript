eb984320e42e1bd62e5c496249e0bd74daceb85b
SHIVAM-XZ <soni.shivam0037@gmail.com>
2024-07-20 17:44:49 +0530

    13 july

---
+++ Day 1.js
@@ -0,0 +1,70 @@
+// Day 1: Variables and Data type
+
+Task1:
+var cars = 4;
+console.log(cars)
+
+Task2:
+let Name= "rahul";
+console.log(Name)
+
+Task3:
+const bool = true;
+console.log(bool)
+
+Task4:
+let number = 4
+let string = "name"
+let bool1 = false
+let object = {
+    name:"shiv",
+    fullname: "gaitonde"
+}
+let array = [1,2,3,4]
+console.log(typeof(number))
+console.log(typeof(string))
+console.log(typeof(bool1))
+console.log(typeof(object))
+console.log(typeof(array))
+
+Task5:
+let num = 2
+console.log(num)
+num = 3
+console.log(num)
+
+Task6:
+const car = "BMW"
+var car = "Audi"
+console.log(car)
+// Identifier 'car' has already been declared
+
+Feature:
+1.
+let number = 4
+let string = "name"
+let bool1 = false
+let object = {
+    name:"shiv",
+    fullname: "gaitonde"
+}
+let array = [1,2,3,4]
+let nul =
+console.log(number ,typeof(number))
+console.log(string, typeof(string))
+console.log(bool1,typeof(bool1))
+console.log(object,typeof(object))
+console.log(array,typeof(array))
+console.log(nul,typeof(nul))
+
+2.
+let number = 4
+number = 5
+console.log(number)
+// return 5
+
+const number = 4
+number = 5
+console.log(number)
+// Assignment to constant variable
+
